8ea6ca6ec2b3944ca2e65780835aa6beba7c2531
mingming.zhou <mingling.zhou@daocloud.io>
2022-10-26 10:54:17 +0800

    init

---
+++ pkg/apis/hwameistor/v1alpha1/annotations.go
@@ -0,0 +1,4 @@
+package v1alpha1
+
+// Annotations are annotations
+type Annotations map[string]string
---
+++ pkg/apis/hwameistor/v1alpha1/labels.go
@@ -0,0 +1,4 @@
+package v1alpha1
+
+// Labels are label for a given daemons
+type Labels map[string]string
